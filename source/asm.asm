61020da1f65a3abb8c45d64179b055223bf1719f
mivimproved <143461207+miVimproved@users.noreply.github.com>
2023-10-23 13:56:43 -0700

    Removed old code

--- source/asm.asm
+++ source/asm.asm
@@ -1,26 +1,10 @@
 ; Vim's Assembly Shenanigans!
 
-section .data
-	; Constants go here
-	; The string that I can print
-	hello_world: db "Hello, standard output!", 0x00
-	hello_world_len: equ $-hello_world     ; Length of hello world
-
-	newline: db 0x0a, 0x00                 ; Newline character
-	newline_len: equ $-newline             ; Length of the newline
-                                               ; Gotten using the amount of data since that.
-	std_err_message: db "This is going to stderr!", 0x0a, 0x00
-	std_err_len: equ $-std_err_message
-
-	userin_msg: db "You did not put a v in?", 0x0a, 0x00
-	userin_msg_len: equ $-userin_msg
-
+section .data ; Constants go here
+	; Strings in "Enter a v"
+	msg_ask_for_v: db "Please enter a 'v' in.", 0x0a, "> ", 0x00
+	msg_not_v: db "You did not put a v in?", 0x0a, 0x00
 	msg_got_v: db "Thank you!", 0xa, 0x00
-	msg_got_v_len: equ $-msg_got_v
-
-	msg_v_tell: db "Please enter a 'v' in.", 0x0a, "> ", 0x00
-	msg_v_tell_len: equ $-msg_v_tell
-
 
 
 	; Syscalls
@@ -33,74 +17,33 @@ section .data
 	std_io: equ 0x1 ; standard output
 	std_err: equ 0x2 ; error output
 
-section .bss
-	userin: resb 100               ; User input array?
-	userin_size: equ $-userin      ; User input array size?
-	; Variables go here
+section .bss ; Variables go here
+	userin: resb 100               ; User input array
+	userin_len: equ $-userin      ; User input array size
 
-section .text
+section .text ; Code goes here
 	global _start
 
-_start_old:
-
-	; Print out Hello, World!
-	mov rax, sys_write             ; System Write
-	mov rdi, std_io                ; Standard Output
-	mov rsi, hello_world           ; Mov the message to write into rsi
-	mov rdx, hello_world_len       ; Move the length of the message into rdx
-	syscall                        ; Kernal call
-
-	; Print out the newline
-	mov rax, sys_write
-	mov rdi, std_io                ; printf("\n");
-	mov rsi, newline
-	mov rdx, newline_len
-	syscall
-
-	; Print out something to stderr
-	mov rax, sys_write
-	mov rdi, std_err
-	mov rsi, std_err_message
-	mov rdx, std_err_len
-	syscall
-
-	; Print out a few newlines
-	mov rax, sys_write
-	mov rdi, std_io
-	mov rsi, newline
-	mov rdx, newline_len
-	syscall
-	syscall                       ; I know I can do this, but most of the time I don't just so I can have more practice with mov
-	
 _start:
 
-
 	; Print out the enter a v message.
-
-	mov rsi, msg_v_tell
+	mov rsi, msg_ask_for_v
 	call print
-
 
 	; Attempt to read something from cin
 	mov rsi, userin                ; Where to put this in.
+	mov rdi, userin_len
 	call getin
-
-
-	; Attempt to write that to the screen
-	mov rsi, userin,
-	mov rdx, 1                     ; Only take the first character
-	; call print
-
 
 	; Check to see if the first character is a 'v'
 	movzx r8, byte [userin]
 	cmp r8, "v"
-	
-	mov r9, userin_msg
+
+	; Move the correct "got" message in
+	mov rsi, msg_got_v
+	mov r9, msg_not_v
 	cmovnz rsi, r9
 	
-	mov r9, msg_got_v
-	cmovz rsi, r9
 
 	call print ; print out that thingie
 
